2845296f70a5c6a29ae920afd7deeda2c498fa0f
VITORIA AZEVEDO DA CRUZ <Vitoria Azevedo>
2024-02-29 17:22:17 -0300

    .

--- app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/calculadoraimc/MainActivity.kt
@@ -3,18 +3,30 @@ package br.senai.sp.jandira.calculadoraimc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -73,7 +85,7 @@ fun Greeting(){
                 )
                 Text(
                     text = "Calculadora IMC",
-                    fontSize = 24.sp,
+                    fontSize = 38.sp,
                     color = Color.White,
                     fontWeight = FontWeight.Bold
                 )
@@ -81,45 +93,138 @@ fun Greeting(){
         }//Tela de dados
         Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .offset(y = (-40).dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
         ){
             Card(
                 modifier = Modifier
                     .width(340.dp)
-                    .height(270.dp)
+                    .height(400.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFFF9F6F6)),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ){
+                Column{
+                    Row(
+                        modifier = Modifier
+                            .padding(40.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Text(
+                            text = "Seus dados",
+                            fontSize = 24.sp,
+                            color = Color(0xFFEA1450),
+                            fontWeight = FontWeight.Black
+                            )
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.SpaceBetween
+
+                    ){
+                        Column{
+                            Text(
+                                text = "Seu peso:",
+                                color = Color(0xFFEA1450)
+                            )
+                            OutlinedTextField(
+                                value = "",
+                                onValueChange = {},
+                                placeholder = {
+                                    Text(
+                                        text = "Seu peso em Kg",
+                                        color = Color(0xFFAFA9A9)
+                                    )
+                                },
+                                shape = RoundedCornerShape(8.dp),
+                                colors = OutlinedTextFieldDefaults.colors(unfocusedBorderColor = Color(0xFFEA1450))
+                            )
+                        }
+                        Column{
+                            Text(
+                                text = "Sua altura:",
+                                color = Color(0xFFEA1450)
+                            )
+                            OutlinedTextField(
+                                value = "",
+                                onValueChange = {},
+                                placeholder = {
+                                    Text(
+                                        text = "Sua altura em cm",
+                                        color = Color(0xFFAFA9A9)
+                                    )
+                                },
+                                shape = RoundedCornerShape(8.dp),
+                                colors = OutlinedTextFieldDefaults.colors(unfocusedBorderColor = Color(0xFFEA1450))
+                            )
+                        }
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 24.dp)
+                                .padding(bottom = 20.dp)
+                                .height(50.dp),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(0xFFEA1450)
+                            ),
+                            shape = RoundedCornerShape(8.dp)
+                        ) {
+                            Text(
+                                text = "CALCULAR",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 14.sp
+                            )
+                        }
+                    }
+                }
+            }
+            Card(
+                modifier = Modifier
+                    .height(120.dp)
+                    .width(340.dp),
+                colors = CardDefaults.cardColors(Color(0xFF329F6B)),
+                border = BorderStroke(1.dp, Color(0xFFAE1450))
             ){
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
+                        .fillMaxWidth()
+                        .fillMaxHeight(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
                 ){
+                    Column(
+                        modifier = Modifier
+                            .height(70.dp),
+                        verticalArrangement = Arrangement.SpaceAround,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Text(
+                            text = "Resultado",
+                            color = Color.White,
+                            fontSize = 14.sp
+                        )
+                        Text(
+                            text = "Peso ideal",
+                            color = Color.White,
+                            fontSize = 20.sp
+                        )
+                    }
                     Text(
-                        text = "Seus dados"
-                    )
-                }
-                Column{
-                    Text(
-                        text = "Seu peso:"
-                    )
-                    TextField(
-                        value = "",
-                        onValueChange = {},
-                        placeholder = {
-                            Text(
-                                text = "Seu peso em Kg"
-                            )
-                        }
-                    )
-                }
-                Column{
-                    Text(
-                        text = "Sua altura:"
+                        text = "21.3",
+                        fontSize = 36.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
                     )
                 }
             }
         }
-
     }
 }
 
